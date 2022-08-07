92dba68ddc85e12c4a62039607ab03d8a0ea07c3
Jace254 <joashmacer@gmail.com>
2022-08-07 19:42:30 +0300

    finished level 2

--- index.mjs
+++ index.mjs
@@ -2,60 +2,111 @@ import {loadStdlib} from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
 const stdlib = loadStdlib(process.env);
 
-const startingBalance = stdlib.parseCurrency(100);
+const runRaffle = async (numTickets) => { //The number of participants in the raffle is assumed to be equal to the number of tickets
+  const startingBalanceA = stdlib.parseCurrency(1000);
+  const startingBalanceB = stdlib.parseCurrency(100);
 
-const [ accAlice, accBob ] =
-  await stdlib.newTestAccounts(2, startingBalance);
-console.log('Hello, Alice and Bob!');
+  console.log(`*     Creating the Alice Account     *`);
 
-console.log('Launching...');
-const ctcAlice = accAlice.contract(backend);
-const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
+  const accAlice = await stdlib.newTestAccount(startingBalanceA);
+  const ctcAlice = accAlice.contract(backend);
 
+  console.log(`*     Creating the Bob Accounts     *`);
 
-console.log(`Alice is launching the NFT`);
-const JSH = await stdlib.launchToken(accAlice, 'Josh', 'NFT', {supply: 1});
-const nftParams = {
-  NFT: JSH.id,
-  ticketAmount: 20,
-}
-const OUTCOME = ["Bob's number is not a match :( ","Bob's number matches ! "]
-await accBob.tokenAccept(JSH.id);
+  const accBobs = await stdlib.newTestAccounts(numTickets, startingBalanceB);
+  const ctcBobs = accBobs.map(B => B.contract(backend, ctcAlice.getInfo()));
+  
+  // creating a new token
+  const supply = 1;
 
-const Shared = {
-  getTicket: (ticketNum) => {
-    const ticket = (Math.floor(Math.random() * ticketNum) + 1);
-    return ticket;
-  },
-  showOutcome: (outcome) => {
-    console.log(OUTCOME[outcome]);
+  const NFT = await stdlib.launchToken(accAlice,"Wukong","NFT",{ supply: supply});
+  accBobs.map(B => B.tokenAccept(NFT));
+  
+  const params = {
+    NFT: NFT.id,
+    ticketAmount: numTickets
   }
+
+  const ticketsPicked = [];
+
+  const printBalances = async (numBobs) => {
+    const printBalance = async (name, acc) => {
+        const [balance, balance_NFT] = await stdlib.balancesOf(acc,[null, NFT]);
+        console.log(`  [+] ${name} has ${fmt(balance)} ${stdlib.standardUnit} and ${balance_NFT} Wukong `);
+    } 
+    await printBalance('Alice', accAlice);
+    for (let i = 0; i < numBobs; i++) {
+        await printBalance(`Bob #${i+1}`, accBobs[i]);
+    }
+  }
+
+  console.log("Starting balances:");
+  await printBalances(numTickets);
+
+  const giveTickets = async () => {
+    
+  }
+
+  const checkWin = async () => {
+    
+  }
+
+  // launch Contract
+  console.log(`Starting Raffle...`)
+  await ctcAlice.p.Alice({
+    ...stdlib.hasRandom,
+    getTicket: () => {
+      const ticket = (Math.floor(Math.random() * numTickets) + 1);
+      return ticket;
+    },
+    setParams: () => {
+      console.log(`Started raffle with the parameters ${params}`)
+      return params;
+    },
+    seeHash: (hash) => {
+      console.log(`The winning number Hash is ${hash}`);
+    },
+    giveTickets: () => {
+      for(const [i, ctc] of ctcBobs.entries()) {
+        const ticket = (Math.floor(Math.random() * numTickets) + 1);
+        const length = ticketsPicked.length();
+        const addr = ctc.getContractAddress()
+        try{
+          while(ticketsPicked.length == length){
+            if(!ticketsPicked.includes(ticket)){
+              await ctc.a.getTicket(ticket);
+              ticketsPicked.push[ticket , addr];
+              console.log(`Bob #${i} picked ticket number ${ticket}`);
+            } else {
+              console.log(`Sorry Bob #${i} that ticket is already picked\nPick another...`);
+            }
+          }
+        } catch(e) {
+          console.log(`All the tickets are picked for the Raffle.`);
+        }
+      }
+    },
+    checkWin: () => {
+      for(const [i, ctc] of ctcBobs.entries()){
+        try {
+          const isWinner = await ctc.a.seeWinner();
+          if(isWinner){
+            console.log(`Bob #${i} has won the Raffle`);
+          } else {
+            console.log(`Better luck next time Bob #${i}`);
+          }
+        } catch(e) {
+          console.log(e);
+        }
+      }
+      console.log("Balances after Raffle:");
+      await printBalances(numTickets);
+    },
+    showOutcome: (Address) => {
+      const index = ticketsPicked.indexOf(Address);
+      console.log(`Bob #${index} won the Raffle with ticket ${ticketsPicked[Address]}`);
+    }
+  });
 }
 
-console.log('Starting backends...');
-await Promise.all([
-  backend.Alice(ctcAlice, {
-    ...stdlib.hasRandom,
-    ...Shared,
-    setParams: () => {
-      return nftParams;
-    },
-    seeHash: (value) => {
-      console.log(`The winning ticket HASH is ${value}`)
-    }
-    // implement Alice's interact object here
-  }),
-  backend.Bob(ctcBob, {
-    ...stdlib.hasRandom,
-    ...Shared,
-    showTicket: (BTicket) => {
-      console.log(`Bob picked Ticket number #${BTicket}`);
-    },
-    seeWinner: (ticket) => {
-      console.log(`The winning ticket is Ticket number #${ticket}`)
-    }
-    // implement Bob's interact object here
-  }),
-]);
-
-console.log('Goodbye, Alice and Bob!');
+await runRaffle(20);
--- index.rsh
+++ index.rsh
@@ -1,12 +1,17 @@
 'reach 0.1';
 
 const amt = 1;
+const duration = 200;
 
-const Shared = {
-  showOutcome: Fun([UInt], Null),
-}
+const [ isOutcome, BLOSE, BWIN ] = makeEnum(2);
+const outcome = (BTicket, WTicket) => ((BTicket == WTicket) ? 1 : 0)
+
+forall(UInt, BTicket =>
+  forall(UInt, WTicket => 
+    assert(isOutcome(outcome(BTicket,WTicket)))));
 
 export const main = Reach.App(() => {
+  setOptions({ untrustworthyMaps: true });
   const A = Participant('Alice', {
     // Specify Alice's interact interface here
     ...hasRandom,
@@ -16,72 +21,87 @@ export const main = Reach.App(() => {
       ticketAmount: UInt,
     })),
     seeHash: Fun([Digest], Null),
+    giveTickets: Fun([], Null),
+    checkWin: Fun([], Null),
+    showOutcome: Fun([Address], Null),
   });
   const B = API('Bob', {
     // Specify Bob's interact interface here
-    showTicket: Fun([UInt], Null),
-    seeWinner: Fun([UInt], Null),
-    showOutcome: Fun([UInt], Null),
+    getTicket: Fun([UInt], Null),
+    seeWinner: Fun([], Bool),
   });
   init();
+
   A.only(() => {
     const { NFT, ticketAmount } = declassify(interact.setParams());
     const _winningTicket = interact.getTicket(ticketAmount);
     const [_commitA, _saltA] = makeCommitment(interact, _winningTicket);
     const commitA = declassify(_commitA)
   });
-  // The first one to publish deploys the contract
+
   A.publish(NFT, ticketAmount, commitA);
-  A.interact.seeHash(commitA)
+  A.interact.seeHash(commitA);
   commit();
   A.pay([[amt, NFT]]);
+  A.interact.giveTickets();
+
+  const pickedTickets = new Map(Address,UInt);
+
+  const [ticketsPicked,isPicking] =
+    parallelReduce([ 0, true ])
+    .invariant(balance(NFT) == amt && balance() == 0)
+    .invariant(ticketsPicked <= ticketAmount)
+    .while(ticketsPicked < ticketAmount && isPicking)
+    .api_(B.getTicket, (ticket) => {
+      return [ 0, (k) => {
+        k(null);
+        pickedTickets[this] = ticket;
+        return [ticketsPicked + 1, true]
+      }]
+    })
+    .timeout(relativeTime(duration), () => {
+      A.publish();
+      return [ ticketsPicked, false ];
+    });
+
   commit();
-
-  const pickedTickets = new Map(Address, UInt);
-
-  const [[ticketAmount], k ] = call(B.getTicket)
-
-  // const [ ticket,ticketsPicked,isPicking] =
-  //   parallelReduce([ 0, 0, true ])
-  //     .invariant(ticketsPicked <= ticketAmount)
-  //     .while(ticketsPicked < ticketAmount && isPicking)
-  //     .api_(B.getTicket, (amt) => {
-  //       check(!pickedTickets.member())
-  //       return [ 0, (k) => {
-  //         k(ticket);
-  //         require(!pickedTickets.member(this,ticket));
-
-  //         return [ticket, ticketsPicked + 1, true]
-  //       }]
-  //     })
-  //      .timeout(DEADLINE, () => {
-  //        TIMEOUT_BLOCK
-  //      });
-  unknowable(B, A(_winningTicket, _saltA));
-
-  B.only(() => {
-    const BTicket = declassify(interact.getTicket(ticketAmount));
-    interact.showTicket(BTicket);
-  })
-  // The second one to publish always attaches
-  B.publish(BTicket);
-  commit();
-
   A.only(() => {
     const saltA = declassify(_saltA);
     const winningTicket = declassify(_winningTicket);
   });
   A.publish(saltA, winningTicket);
   checkCommitment(commitA, saltA, winningTicket);
+  A.interact.checkWin();
 
-  B.interact.seeWinner(winningTicket);
 
-  const outcome = BTicket == winningTicket ? 1 : 0;
+  const [winner, remainingB ] = 
+    parallelReduce([ A, ticketsPicked ])
+    .invariant(balance(NFT) == amt && balance() == 0)
+    .invariant(remainingB <= ticketsPicked)
+    .while(remainingB > 0)
+    .api(B.seeWinner, 
+      (k) => {
+        const BTicket = fromSome(pickedTickets[this],0);
+        assert(outcome(BTicket,winningTicket) == BWIN ||
+              outcome(BTicket,winningTicket) == BLOSE);
 
-  transfer([[amt,NFT]]).to(outcome == 0 ? A : B);
-  each([A, B], () => interact.showOutcome(outcome));
+        if(outcome(BTicket,winningTicket) == BWIN){
 
+          k(true);
+          delete pickedTickets[this];
+          return [this, remainingB -1];
+
+        } else {
+
+          k(false);
+          delete pickedTickets[this];
+          return [ A , remainingB -1 ];
+
+        }
+      })
+
+  transfer(amt,NFT).to(winner);
+  A.interact.showOutcome(winner);
   commit();
-  // write your program here
   exit();
 });
